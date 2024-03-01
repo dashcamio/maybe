ade8ee3022ca4c654261011dc3778703a1f9336a
Ian Jennings <ian@meetjennings.com>
2024-03-01 08:35:55 -0600

    try to build and test

---
+++ .testdriver/prerun.sh
@@ -0,0 +1,15 @@
+brew install rbenv ruby-build
+rbenv init
+rbenv install 3.3.0
+rbenv global 3.3.0
+ruby -v
+brew install postgresql
+brew services start postgresql
+cd maybe
+cp .env.example .env
+bundle install
+bin/rails db:setup
+bin/dev &
+npm install dashcam-chrome --save
+/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --load-extension=./node_modules/dashcam-chrome/build/ 1>/dev/null 2>&1 &
+exit
